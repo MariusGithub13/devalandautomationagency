3790bef88d921e9ff6e4f0f38e782ddd8cc64212
Marius <42419630+MariusGithub13@users.noreply.github.com>
2025-11-21 17:55:19 +0000

    Wrap SOL and SAL footer logos with external links (open in new tab)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -176,8 +176,12 @@ export default function Footer() {
             </ul>
               {/* ANPC/SOL/SAL logos */}
               <div className="mt-6 flex gap-4 items-center">
-                <img src="/images/sol.png" alt="SOL" className="h-12 object-contain" />
-                <img src="/images/sal.png" alt="SAL" className="h-12 object-contain" />
+                <a href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=RO" target="_blank" rel="noopener noreferrer" className="inline-block">
+                  <img src="/images/sol.png" alt="SOL" className="h-12 object-contain" />
+                </a>
+                <a href="https://reclamatiisal.anpc.ro/" target="_blank" rel="noopener noreferrer" className="inline-block">
+                  <img src="/images/sal.png" alt="SAL" className="h-12 object-contain" />
+                </a>
               </div>
           </div>
         </div>
